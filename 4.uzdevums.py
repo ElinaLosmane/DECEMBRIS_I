308c96d1a9663b0d43331f4e5bdf62c47c88ae8d
Elina <elina.losmane@grobinasskola.lv>
2023-11-24 10:22:23 +0200

    Izveidots 4.uzdevums

---
+++ 4.uzdevums.py
@@ -0,0 +1,11 @@
+ievaditais_skaitlis = int(input("Ievadiet skaitli:"))
+
+# Piešķir vērtību faktoriālam
+faktorials = 1
+
+# Aprēķinu faktoriālu, izmantojot for ciklu
+for skaitlis in range(1, ievaditais_skaitlis + 1):
+    faktorials*= skaitlis
+
+# Izvada rezultātu
+print(f"Faktoriāls no {ievaditais_skaitlis} ir: {faktorials}")
